b0dc32027ff4b7720e0a8c0e793be5ba18b20fa5
nkarl <21993921+nkarl@users.noreply.github.com>
2022-12-31 20:50:44 -0800

    fixed dashboard location.

--- frontend/src/components/dashboard/dashboard.js
+++ frontend/src/components/dashboard/dashboard.js
@@ -2,3 +2,9 @@ import {Component} from 'react';
 import './dashboard.css';
 import logo from 'assets/img/logo.svg';
 import { AxiosInstance } from '../../util/axios-api';
+
+export const Dashboard = () => {
+  return (
+    <h1>THIS IS THE DASHBOARD.</h1>
+  )
+}
