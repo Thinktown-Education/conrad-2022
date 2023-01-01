1c0266e52bae9d58779a5d621a337f8447513054
nkarl <21993921+nkarl@users.noreply.github.com>
2022-12-31 20:34:27 -0800

    Trying to redirect from login to dashboard.

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -2,6 +2,41 @@ import { Component } from 'react';
 import './login.css';
 import logo from 'assets/img/logo.svg';
 import { AxiosInstance } from '../../util/axios-api';
+import { BrowserRouter, BrowserRouter as Routes, Route } from 'react-router-dom';
+import { Dashboard } from '../dashboard/dashboard';
+import { Axios } from 'axios';
+
+function signInUser() {
+    const email = "aaa@aaa.com"
+    const password = "abcd1234"
+    // const email = document.getElementById('email-input').value
+    // const password = document.getElementById('password-input').value
+    AxiosInstance.post("http://localhost:8080/auth/login", {
+        email: email,
+        password: password,
+    }).then((response) => {
+        console.log(response.data)
+        // this.setState({
+        //     text: response.data
+        // })
+    })
+    
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='../dashboard/dashboard' component={Dashboard} />
+            </Routes>
+        </BrowserRouter>
+    )
+    // window.location = 'http://localhost:3000/dashboard'
+    // window.location = 'https://example.com'
+    // AxiosInstance.get("http://localhost:8080/mindmap/test").then((response) => {
+    //     console.log(response.data)
+        // this.setState({
+        //     text: response.data
+        // })
+    // })
+}
 
 const Login = () => {
     return (
@@ -25,13 +60,13 @@ const Login = () => {
                     <input id='remember-user' type='checkbox' />Remember me
                 </label>
             </form>
-            <button id='signin' type='button'>SIGN IN</button>
+            <button id='signin' type='button' onClick={signInUser}>SIGN IN</button>
             <div id='account-retrieval'>
                 <a id='forgot-password' href='#'>Forgot Password?</a>
                 <a id='register-account' href='#'>Register</a>
             </div>
         </div>
-    )
+    )    
 }
 
 export default Login;
